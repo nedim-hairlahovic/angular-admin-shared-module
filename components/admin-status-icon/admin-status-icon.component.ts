3d500427e1346ece6d4553eeb1665555d004737f
Nedim Hairlahović <nedim_hairlahovic@hotmail.de>
2026-03-12 14:59:50 +0100

    Add false label to AdminStatusIconComponent

--- components/admin-status-icon/admin-status-icon.component.ts
+++ components/admin-status-icon/admin-status-icon.component.ts
@@ -1,17 +1,21 @@
-import { Component, Input } from '@angular/core';
-import { TooltipDirective } from '../../directives/tooltip.directive';
+import { Component, Input } from "@angular/core";
+
+import { TooltipDirective } from "../../directives/tooltip.directive";
 
 @Component({
-  selector: 'admin-status-icon',
+  selector: "admin-status-icon",
   standalone: true,
   imports: [TooltipDirective],
   template: `
     @if (value) {
       <i class="fa fa-check-circle text-success ms-1" [tooltip]="title"></i>
+    } @else if (falseLabel) {
+      <span>{{ falseLabel }}</span>
     }
   `,
 })
 export class AdminStatusIconComponent {
   @Input() value: boolean = false;
-  @Input() title: string = '';
+  @Input() title: string = "";
+  @Input() falseLabel: string = "";
 }
